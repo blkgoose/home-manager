7b88788475a31b2add465ff5d02de6c361eab92e
blkgoose <alessio.biancone@gmail.com>
2026-04-17 13:04:58 +0200

    feat: add extraConfig option

--- modules/services/keynav.nix
+++ modules/services/keynav.nix
@@ -14,12 +14,31 @@ in
     enable = lib.mkEnableOption "keynav";
 
     package = lib.mkPackageOption pkgs "keynav" { };
+
+    extraConfig = lib.mkOption {
+      type = lib.types.attrsOf lib.types.str;
+      default = { };
+      description =
+        "Configuration for keynav written to {file}`~/.keynavrc`. Each attribute name is a key binding and the value is the action. See <https://github.com/jordansissel/keynav/blob/master/keynav.pod> for available bindings and actions.";
+      example = {
+        "2" = "doubleclick,end";
+        "4" = "click 4";
+        "5" = "click 5";
+      };
+    };
   };
 
   config = lib.mkIf cfg.enable {
     assertions = [
       (lib.hm.assertions.assertPlatform "services.keynav" pkgs lib.platforms.linux)
     ];
+
+    home.file.".keynavrc" = lib.mkIf (cfg.extraConfig != { }) {
+      text = lib.concatStringsSep "\n"
+        (lib.mapAttrsToList (key: value: "${key} ${value}") cfg.extraConfig)
+        + "\n";
+      onChange = "systemctl --user restart keynav.service || true";
+    };
 
     systemd.user.services.keynav = {
       Unit = {
